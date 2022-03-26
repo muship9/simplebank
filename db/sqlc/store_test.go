90cffeacb7921f4d19e18e2ac7207dccecdb0d55
shinp09 <57585657+shinp09@users.noreply.github.com>
2022-03-26 21:04:06 +0900

    feat: table

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,10 @@
+package db
+
+import "testing"
+
+func TestStore_TransferTx(t *testing.T) {
+	//store := NewStore(testDB)
+	//
+	//account1 := createRandomAccount(t)
+	//account2 := createRandomAccount(t)
+}
